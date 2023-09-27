f019ce38edb6379794d3022ee2bf34132ae53fa2
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-09-27 13:39:31 +0100

    adding models

---
+++ models/character.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Learn.models
+{
+    public class character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Richard";
+        public int HtPoint { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+    }
+}
