d064d90dd5efaccc08d9be5d025df12237a9eb05
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-10-10 17:10:14 +0100

    resuming learning

--- models/character.cs
+++ models/character.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
 
 namespace ASP.NET_Learn.models
 {
@@ -24,3 +25,16 @@ namespace ASP.NET_Learn.models
         }
     }
 }
+
+namespace Recap
+{
+    public class PlayerStat
+    {
+        public int Ide {get; set;}
+        public int MyProperty { get; set; } = 100;
+        
+        public string Name {get;set;} = "Japos";
+    }
+}
+
+
